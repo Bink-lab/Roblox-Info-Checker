122d46a9b9c3f51be2cfce039fe6e8ce98d58216
Bink <91280973+Bink-lab@users.noreply.github.com>
2024-03-19 11:25:24 +0000

    d

--- project/project.go
+++ project/project.go
@@ -1,49 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "net/http"
-    "os"
+	"fmt"
+	"time"
 )
 
+// main.go is the entry point for the application.
+// It prints "Hello, World!" to the console and then sleeps for 10 seconds.
+
 func main() {
-    // Ask the user to enter the URL and file name
-    fmt.Print("Enter the URL: ")
-    var url string
-    fmt.Scanln(&url)
-    fmt.Print("Enter the file name: ")
-    var extensie string
-    fmt.Scanln(&extensie)
-    
-    // Check if the URL is valid
-    if _, err := http.ParseURL(url); err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
+	fmt.Print("Hello, World!")
+	time.Sleep(10 * time.Second)
 
-    // Send HTTP GET request to the URL
-    response, err := http.Get(url)
-    if err != nil {
-        fmt.Println("Error:", err) 
-        return
-    }
-    defer response.Body.Close()
+	// wait for clean ctrl + c clean exit then type exiting
+	<-make(chan bool)
 
-    // Create a new file to save the downloaded content
-    outputFile, err := os.Create(extensie + ".txt") // Append ".txt" extension to the file name
-    if err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
-    defer outputFile.Close()
-
-    // Copy the response body to the file
-    _, err = io.Copy(outputFile, response.Body)
-    if err != nil {
-        fmt.Println("Error:", err)
-        return
-    }
-
-    fmt.Println("File downloaded successfully!")
+	fmt.Println("Exiting")
 }
